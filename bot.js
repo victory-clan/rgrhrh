85637adafb5c401489b74de40aae0a92dbb647de
victory-clan <46355716+victory-clan@users.noreply.github.com>
2019-01-31 22:56:27 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const iKhaled = ['403516759228809218','403516759228809218'];
+
+
+  client.on('message', Khaled => {
+    var argresult = Khaled.content.split(` `).slice(1).join(' ');
+      if (!iKhaled.includes(Khaled.author.id)) return;
+  
+      if (Khaled.content.startsWith('Nwt')) {
+        client.user.setActivity(argresult, {type:'WATCHING'});
+          client.user.setStatus("dnd")
+          Khaled.channel.send(` ** Done. :white_check_mark: ** `)
+      } else //Khaled 
+      if (Khaled.content.startsWith('Nls')) {//Khaled
+          client.user.setActivity(argresult , {type:'LISTENING'});
+            client.user.setStatus("idle")
+            Khaled.channel.send(` ** Done. :white_check_mark: ** `)
+      } else //Khaled
+          if (Khaled.content.startsWith('N$st')) {
+            client.user.setGame(argresult, "https://www.twitch.tv/ikhaled321");
+              client.user.setStatus("dnd")
+              Khaled.channel.send(` ** Done. :white_check_mark: ** `)//Khaled
+      } else 
+      if (Khaled.content.startsWith('Npl')) {
+        client.user.setActivity(argresult , {type:'PLAYING'});//Khaled
+         client.user.setStatus("idle")
+         Khaled.channel.send(` ** Done. :white_check_mark: ** `)
+       }
+  
+  });
+
+client.login(process.env.BOT_TOKEN);
